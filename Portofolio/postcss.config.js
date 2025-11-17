4e29a279cc9165a7d98b657e749bb206ce0bdb17
FaizGhazali0712 <185329515+FaizGhazali0712@users.noreply.github.com>
2025-11-17 11:27:36 +0700

    export

--- Portofolio/postcss.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plugins: {
-    // Ganti tailwindcss dengan @tailwindcss/postcss
-    '@tailwindcss/postcss': {}, 
-    // Di v4, Autoprefixer tidak lagi diperlukan karena sudah ditangani secara otomatis
-  },
-}
